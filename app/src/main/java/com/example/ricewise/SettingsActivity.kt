5f58e561d65f5829e54aaaedd9aa268e5647aaf1
cristalmhel <cristalmhelrequierme@gmail.com>
2025-05-05 09:34:44 +0800

    commit 05-05-2025 v2
    commit 05-05-2025 v2

--- app/src/main/java/com/example/ricewise/SettingsActivity.kt
+++ app/src/main/java/com/example/ricewise/SettingsActivity.kt
@@ -1,7 +1,9 @@
 package com.example.ricewise
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,5 +13,11 @@ class SettingsActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val backToHome = findViewById<TextView>(R.id.backToHome)
+        backToHome.setOnClickListener {
+            startActivity(Intent(this, DashboardActivity::class.java))
+            finish()
+        }
     }
 }
